95b0e7cd91c64f7ae9b0e693499035651be46dc3
arnoj <john.arnold@pega.com>
2022-12-22 13:38:50 -0500

    Get build/start working and modify lib component map as needed

--- README.md
+++ README.md
@@ -8,4 +8,27 @@ can modify, replace, or add components to suit their specific needs.
 
 <br>
 
+# Normal workflow
+
+1.  **npm run ci** - cleans up all generated files from previous use and 
+installs all of the npm packages needed
+
+1.  **npm run build:dev** - runs lint and a development build
+
+1.  **npm run start-dev** - starts a development server (with live reload)
+
+1.  **Work in VS Code** - live reload should allow you to see changes in browser
+
+1.  Once you're ready to build a new packaging of the SDK components. <br>
+**npm run build-sdk** - compiles all of the necessary code and puts the contents of the npm packaging in **packages/react-sdk-components/lib**
+
+
+
+1.  Then, when you want to create the .tgz file, <br>
+**npm run pack-sdk** - creates the .tgz file of the compiled code in **packages/react-sdk-components/lib**
+
+
+<br>
+<hr>
+<br>
 _more later..._
---
+++ scripts/edit-pega-components-map-in-lib.js
@@ -0,0 +1,48 @@
+// @ts-nocheck
+
+// NOTE: The @ts-nocheck above tells TypseScript not to check this file for various problems
+
+// Doc for replace-in-file package here:
+//  https://www.npmjs.com/package/replace-in-file?activeTab=readme
+
+'use strict';
+
+const replaceInFile = require('replace-in-file');
+
+const editPegaComponentsMapInLib = function() {
+  console.log(`in editPegaComponentsMapInLib`);
+
+  const compDtsFileToEdit = 'packages/react-sdk-components/lib/sdk-pega-component-map.d.ts';
+  const compDtsMapFileToEdit = 'packages/react-sdk-components/lib/sdk-pega-component-map.d.ts.map';
+  const compJsFileToEdit = 'packages/react-sdk-components/lib/sdk-pega-component-map.js';
+  const compJSMapFileToEdit = 'packages/react-sdk-components/lib/sdk-pega-component-map.js.map';
+  
+
+  const options = {
+    files: [compJsFileToEdit, compJSMapFileToEdit, compDtsFileToEdit, compDtsMapFileToEdit],
+    from: /\/src\//g,
+    to: "/lib/",
+    countMatches: true
+  }
+
+  try {
+    const results = replaceInFile.sync(options);
+    results.map((theResults) => {
+      const { hasChanged, file, numMatches, numReplacements } = theResults;
+      // console.log(`replacement results: ${JSON.stringify(results[0])}`);
+      if (hasChanged) {
+        console.log(`edited: ${file}: found ${numMatches} | replaced ${numReplacements}`);
+      } else {
+        console.log(`NOT edited: ${file}: found ${numMatches} | replaced ${numReplacements}`);
+      }
+  
+    });
+
+  }
+  catch (error) {
+    console.error(`ERROR in edit: ${error}`);
+  }
+
+}
+
+editPegaComponentsMapInLib();
