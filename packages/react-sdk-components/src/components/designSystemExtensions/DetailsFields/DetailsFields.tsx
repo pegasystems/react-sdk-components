34bd75b214b66d1a5cecbe6675818d25f637fffd
arnoj <john.arnold@pega.com>
2023-01-05 15:38:21 -0500

    Next batch of updates

--- packages/react-sdk-components/src/components/designSystemExtensions/DetailsFields/DetailsFields.tsx
+++ packages/react-sdk-components/src/components/designSystemExtensions/DetailsFields/DetailsFields.tsx
@@ -5,7 +5,6 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
-// import createPConnectComponent from '../../../bridge/react_pconnect';
 import createPConnectComponent from '../../../bridge/react_pconnect';
 
 import { format } from '../../helpers/formatters';
