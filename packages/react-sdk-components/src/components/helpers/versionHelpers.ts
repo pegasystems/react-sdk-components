414056aeea8d1b71b641bbbb4a88aee840b1f904
arnoj <john.arnold@pega.com>
2023-01-06 16:40:16 -0500

    20230106 Updates to samples and helpers files

--- packages/react-sdk-components/src/components/helpers/versionHelpers.ts
+++ packages/react-sdk-components/src/components/helpers/versionHelpers.ts
@@ -5,15 +5,15 @@
  * PCore/PConnect is being run
  */
 
- declare const PCore;
+declare const PCore;
 
- export const sdkVersion = "8.7";
- 
- export function compareSdkPCoreVersions() {
- 
-   // const theConstellationVersion = PCore.getPCoreVersion();
- 
-   // eslint-disable-next-line no-console
-   console.warn(`Using Constellation version ${PCore.getPCoreVersion()}. Ensure this is the same version as your Infinity server.`);
- 
- }
+export const sdkVersion = "8.7";
+
+export function compareSdkPCoreVersions() {
+
+  // const theConstellationVersion = PCore.getPCoreVersion();
+
+  // eslint-disable-next-line no-console
+  console.warn(`Using Constellation version ${PCore.getPCoreVersion()}. Ensure this is the same version as your Infinity server.`);
+
+}
