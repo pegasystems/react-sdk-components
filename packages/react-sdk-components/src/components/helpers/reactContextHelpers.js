0ca22df8fe68545779f5675859c9be11845fb659
arnoj <john.arnold@pega.com>
2023-02-15 16:21:32 -0500

    Fix override of AppShell and NavBar

---
+++ packages/react-sdk-components/src/components/helpers/reactContextHelpers.js
@@ -0,0 +1,10 @@
+import { createContext, useContext } from "react";
+
+// This file contains a shared access point for any React contexts that may need to be
+//  defined/used across various components.
+
+//  For example, the AppShell wants to use a NavContext that interacts with
+//  that context's consumption in the NavBar to handle clicks for opening/closing
+//  aspects of the NavBar
+export const NavContext = createContext({open: false, setOpen: f => f});
+export const useNavBar = () => useContext(NavContext);
