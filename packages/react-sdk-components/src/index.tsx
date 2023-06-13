2fe6c258db420ae86a85dad3d009a4251eaba8f7
Siva Rama Krishna <sivaramakrishna.tumma@in.pega.com>
2023-06-13 15:26:51 +0530

    Avoid blank display for wrong portal login

--- packages/react-sdk-components/src/index.tsx
+++ packages/react-sdk-components/src/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from 'react-router-dom';
 import TopLevelApp from '../src/samples/TopLevelApp';
+import './common.css';
 
 const outletElement = document.getElementById("outlet");
 
