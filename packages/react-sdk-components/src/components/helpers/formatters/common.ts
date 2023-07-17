45ca16f8b39e82f8e2f835ea6add37b020ec086d
vishal sharma <vishalshrm539@gmail.com>
2023-07-17 17:17:04 +0530

    Replace getUseLocal to getLocal to get the latest local value for the operator

--- packages/react-sdk-components/src/components/helpers/formatters/common.ts
+++ packages/react-sdk-components/src/components/helpers/formatters/common.ts
@@ -2,7 +2,7 @@ export function getLocale(locale='') {
   // use locale if specified
   if (locale) return locale;
   // otherwise, use operator locale if it's defined
-  if (PCore.getEnvironmentInfo().getUseLocale()) return PCore.getEnvironmentInfo().getUseLocale();
+  if (PCore.getEnvironmentInfo().getLocale()) return PCore.getEnvironmentInfo().getLocale();
   // fallback
   return Intl.DateTimeFormat().resolvedOptions().locale;
 }
