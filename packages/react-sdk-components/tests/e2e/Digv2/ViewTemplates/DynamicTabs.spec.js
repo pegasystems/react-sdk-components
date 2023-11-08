9811c75561615983df7ef150a7d2266da5395006
Siva Rama Krishna <sivaramakrishna.tumma@in.pega.com>
2023-11-08 20:02:22 +0530

    Dynamic Tabs support added

---
+++ packages/react-sdk-components/tests/e2e/Digv2/ViewTemplates/DynamicTabs.spec.js
@@ -0,0 +1,71 @@
+/* eslint-disable no-template-curly-in-string */
+/* eslint-disable no-undef */
+
+const { test, expect } = require('@playwright/test');
+import { attachCoverageReport } from 'monocart-reporter';
+
+const config = require('../../../config');
+const common = require('../../../common');
+
+test.beforeEach(async ({ page }) => {
+  await page.setViewportSize({ width: 1720, height: 1080 });
+  await page.goto('http://localhost:3502/portal', { waitUntil: 'networkidle' });
+});
+
+test.describe('E2E test', () => {
+  test('should login, create case and run different test cases for Dynamic Tabs', async ({ page }) => {
+    await common.Login(config.config.apps.digv2.user.username, config.config.apps.digv2.user.password, page);
+
+    /** Testing announcement banner presence */
+    const announcementBanner = page.locator('h6:has-text("Announcements")');
+    await expect(announcementBanner).toBeVisible();
+
+    /** Testing worklist presence */
+    const worklist = page.locator('h6:has-text("My Worklist")');
+    await expect(worklist).toBeVisible();
+
+    /** Creating a View Templates case-type */
+    const complexFieldsCase = page.locator('div[role="button"]:has-text("View Templates")');
+    await complexFieldsCase.click();
+
+    /** Selecting Confirmation from the Category dropdown */
+    const selectedCategory = page.locator('div[data-test-id="76729937a5eb6b0fd88c42581161facd"]');
+    await selectedCategory.click();
+    await page.locator('li:has-text("Dynamic Tabs")').click();
+
+    await page.locator('button:has-text("submit")').click();
+
+    const title = await page.locator('h3[id="dynamic-tabs-title"]');
+    await expect(title).toBeVisible();
+    await expect(await title.innerText()).toBe('Dynamic Tabs Template');
+
+    const tablist = await page.locator('div[id="dynamic-tabs"] div[role="tablist"]');
+    const tabpanel = await page.locator('div[id="dynamic-tabpanel"]');
+
+    await expect(tabpanel.nth(0).locator('span:has-text("Make")')).toBeVisible();
+    await expect(tabpanel.nth(0).locator('span:has-text("BMW")')).toBeVisible();
+
+    await tablist.locator('button').nth(1).click();
+    expect(await tabpanel.nth(1).locator('span:has-text("Make")')).toBeVisible();
+    expect(await tabpanel.nth(1).locator('span:has-text("Audi")')).toBeVisible();
+
+    await tablist.locator('button').nth(2).click();
+    expect(await tabpanel.nth(2).locator('span:has-text("Make")')).toBeVisible();
+    expect(await tabpanel.nth(2).locator('span:has-text("FIAT")')).toBeVisible();
+
+    await tablist.locator('button').nth(3).click();
+    expect(await tabpanel.nth(3).locator('span:has-text("Make")')).toBeVisible();
+    expect(await tabpanel.nth(3).locator('span:has-text("Chevrolet")')).toBeVisible();
+  }, 10000);
+});
+
+test.afterEach(async ({ page }) => {
+  const coverageData = await page.evaluate(() => window.__coverage__);
+  expect(coverageData, 'expect found Istanbul data: __coverage__').toBeTruthy();
+  // coverage report
+  const report = await attachCoverageReport(coverageData, test.info(), {
+    outputDir: "./test-reports/e2e/DigV2/ViewTemplates/DynamicTabs"
+  });
+  console.log(report.summary);
+  await page.close();
+});
