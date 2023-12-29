c7e75ecb669e89c9f2295247e09e159783d2794c
mashm <manasa.mashetty@in.pega.com>
2023-12-29 16:43:17 +0530

    updated as per PR comments

--- packages/react-sdk-components/src/components/infra/Containers/containerHelpers.ts
+++ packages/react-sdk-components/src/components/infra/Containers/containerHelpers.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/prefer-default-export
 export const isContainerInitialized = (pConnect) => {
   const context = pConnect.getContextName();
   const containerName = pConnect.getContainerName();
