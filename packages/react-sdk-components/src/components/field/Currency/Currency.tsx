8839336b6bd453e5578d6ae9a7f876a838fee588
tumms2021389 <97188066+tumms2021389@users.noreply.github.com>
2024-01-03 00:38:02 +0530

    fix: currency formatting display doesn't stay consistent (#265)

--- packages/react-sdk-components/src/components/field/Currency/Currency.tsx
+++ packages/react-sdk-components/src/components/field/Currency/Currency.tsx
@@ -37,24 +37,16 @@ export default function Currency(props: CurrrencyProps) {
 
   const pConn = getPConnect();
   const actions = pConn.getActionsApi();
-  const propName = pConn.getStateProps()["value"];
+  const propName = pConn.getStateProps()['value'];
   const helperTextToDisplay = validatemessage || helperText;
 
   // console.log(`Currency: label: ${label} value: ${value}`);
 
-  let readOnlyProp = {}; // Note: empty if NOT ReadOnly
-
-  if (readOnly) {
-    readOnlyProp = { readOnly: true };
-  }
-
-  let testProp = {};
-
-  testProp = {
+  const testProp = {
     'data-test-id': testId
   };
 
-  const [currValue, setCurrValue] = useState('');
+  const [currValue, setCurrValue] = useState(value.toString());
   const [theCurrSym, setCurrSym] = useState('$');
   const [theCurrDec, setCurrDec] = useState('.');
   const [theCurrSep, setCurrSep] = useState(',');
@@ -67,11 +59,6 @@ export default function Currency(props: CurrrencyProps) {
     setCurrSep(theSymbols.theDigitGroupSeparator);
   }, [currencyISOCode]);
 
-  useEffect(() => {
-    // const testVal = value;
-    setCurrValue(value.toString());
-  }, [value]);
-
   const theCurrencyOptions = getCurrencyOptions(currencyISOCode);
   const formattedValue = format(value, pConn.getComponentName().toLowerCase(), theCurrencyOptions);
 
@@ -83,8 +70,7 @@ export default function Currency(props: CurrrencyProps) {
     return <FieldValueList name={hideLabel ? '' : label} value={formattedValue} variant="stacked" />;
   }
 
-  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-  function currOnChange(event, inValue) {
+  function currOnChange(event) {
     // console.log(`Currency currOnChange inValue: ${inValue}`);
 
     // update internal value
@@ -107,18 +93,19 @@ export default function Currency(props: CurrrencyProps) {
       size="small"
       required={required}
       disabled={disabled}
-      onChange={currOnChange}
-      onBlur={!readOnly ? currOnBlur : undefined}
+      readOnly={!!readOnly}
       error={status === 'error'}
       label={label}
       value={currValue}
       type="text"
       outputFormat="number"
       textAlign="left"
-      InputProps={{ ...readOnlyProp, inputProps: { ...testProp, value: currValue } }}
+      InputProps={{ inputProps: { ...testProp, value: currValue } }}
       currencySymbol={theCurrSym}
       decimalCharacter={theCurrDec}
       digitGroupSeparator={theCurrSep}
+      onChange={currOnChange}
+      onBlur={!readOnly ? currOnBlur : undefined}
     />
   );
 }
