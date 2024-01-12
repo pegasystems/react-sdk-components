8985e74234a2073a361908e7280ea3dd353024ab
Siva Rama Krishna <sivaramakrishna.tumma@in.pega.com>
2024-01-12 13:48:22 +0530

    scripts update and formatOnSave enabled

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,6 @@
+// Prettier config options: https://prettier.io/docs/en/options.html
+// Shared front-end config: https://git.pega.io/projects/FE/repos/configs/browse/packages/prettier-config/index.json
+
 const pegaPrettierConfig = require('@pega/prettier-config');
 
 module.exports = {
