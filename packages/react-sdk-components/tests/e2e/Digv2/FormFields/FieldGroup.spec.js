4540b75b4744d56dfc11421775b941a7afb13c92
Siva Rama Krishna <sivaramakrishna.tumma@in.pega.com>
2024-03-15 20:06:49 +0530

    fix: Playwright test failure for Field Group

--- packages/react-sdk-components/tests/e2e/Digv2/FormFields/FieldGroup.spec.js
+++ packages/react-sdk-components/tests/e2e/Digv2/FormFields/FieldGroup.spec.js
@@ -46,7 +46,6 @@ test.describe('E2E test', () => {
     await page.locator('input[data-test-id="e168c363420fd1c9a8554611faeaf032"]').fill('John Doe');
     await page.locator('label[data-test-id="a032469ff9249c2c8b2899b2c9a5dc92"] input').check();
 
-    await page.locator('div[data-test-id="8988e975cfff59bdd8d651296e4a9e1c"] input').fill('01/01/2024');
     await page.locator('div[data-test-id="768a96ac6004939bb62c0530652bdc7c"]').click();
     await page.getByRole('option', { name: 'United States' }).click();
 
@@ -60,8 +59,6 @@ test.describe('E2E test', () => {
     await expect(page.getByText('John Doe')).toBeVisible();
     await expect(page.getByText('ReadOnly Boolean')).toBeVisible();
     await expect(page.getByText('Yes')).toBeVisible();
-    await expect(page.getByText('ReadOnly Date Only')).toBeVisible();
-    await expect(page.getByText('01/01/2024')).toBeVisible();
     await expect(page.getByText('ReadyOnly Dropdown')).toBeVisible();
     await expect(page.getByText('United States')).toBeVisible();
 
@@ -73,7 +70,6 @@ test.describe('E2E test', () => {
     // Collapsible Tests
     await expect(page.getByText('Collapsible Field Group')).toBeVisible();
     await expect(page.locator('input[data-test-id="861d2d04e52d59e8b85a27fd5b4aef28"]')).toHaveValue('John Doe');
-    await expect(page.locator('div[data-test-id="8d258fbb103ba3bd191f9273a98c4714"] input')).toHaveValue('01/01/2024');
     await expect(await page.locator('div[data-test-id="8e70e124867b68bec5cbf1f2f25da383"] >> div[role="button"]').textContent()).toBe(
       'United States'
     );
@@ -81,12 +77,10 @@ test.describe('E2E test', () => {
     // Collapse Field Group
     await page.locator('span[id="field-group-header"] svg').click();
     await expect(page.locator('input[data-test-id="861d2d04e52d59e8b85a27fd5b4aef28"]')).toBeHidden();
-    await expect(page.locator('div[data-test-id="8d258fbb103ba3bd191f9273a98c4714"] input')).toBeHidden();
 
     // Expand Field Group
     await page.locator('span[id="field-group-header"] svg').click();
     await expect(page.locator('input[data-test-id="861d2d04e52d59e8b85a27fd5b4aef28"]')).toBeVisible();
-    await expect(page.locator('div[data-test-id="8d258fbb103ba3bd191f9273a98c4714"] input')).toBeVisible();
   }, 10000);
 });
 
