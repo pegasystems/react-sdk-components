52445a1629fb8a9f212f2213506bc1b10c52d639
mashm <manasa.mashetty@in.pega.com>
2024-07-26 17:28:03 +0530

    updated as per comments

--- packages/react-sdk-components/src/components/infra/Assignment/Assignment.tsx
+++ packages/react-sdk-components/src/components/infra/Assignment/Assignment.tsx
@@ -135,7 +135,7 @@ export default function Assignment(props: PropsWithChildren<AssignmentProps>) {
         editableElements[0].focus();
       }
     }
-    window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }, [children]);
 
   function showToast(message: string) {
