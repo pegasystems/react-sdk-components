fde87cb3b73c1f1a199b9bed71db184de6d8ff01
mashm <manasa.mashetty@in.pega.com>
2024-10-17 18:31:15 +0530

    semanticURL changes

--- packages/react-sdk-components/src/samples/TopLevelApp/index.tsx
+++ packages/react-sdk-components/src/samples/TopLevelApp/index.tsx
@@ -18,10 +18,10 @@ document.addEventListener('SdkLoggedOut', () => {
     theLogoutMsgDiv.innerHTML = `You are logged out. Refresh the page to log in again.`;
     thePegaRoot.appendChild(theLogoutMsgDiv);
   }
-  window.location.href = `${window.location.origin}/portal`;
   sessionStorage.removeItem('logined');
   sessionStorage.removeItem('redirected');
   sessionStorage.removeItem('rsdk_portalName');
+  window.location.href = `${window.location.origin}/portal`;
 });
 
 export default TopLevelApp;
