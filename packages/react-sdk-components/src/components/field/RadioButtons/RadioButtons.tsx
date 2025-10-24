ecfd71ab2de3883f12310875c7e9bbd4eabcdff6
tammy-a11y <yuvanagasai.tammu@in.pega.com>
2025-10-24 17:08:06 +0530

    SDK-R Fixed text alignment in selectable card component (#499)
    Co-authored-by: tumms2021389 <97188066+tumms2021389@users.noreply.github.com>

--- packages/react-sdk-components/src/components/field/RadioButtons/RadioButtons.tsx
+++ packages/react-sdk-components/src/components/field/RadioButtons/RadioButtons.tsx
@@ -108,10 +108,7 @@ export default function RadioButtons(props: RadioButtonsProps) {
     return (
       <div>
         <h4 style={{ marginTop: 0, marginBottom: 0 }}>{label}</h4>
-        <div
-          className='selectable-card-grid-style'
-          style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(min(100%, 40ch), 1fr))', gridAutoRows: '1fr', gap: '0.5rem' }}
-        >
+        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(min(30%, 40ch), 1fr))', gridAutoRows: '1fr', gap: '0.5rem' }}>
           <SelectableCard
             hideFieldLabels={hideFieldLabels}
             additionalProps={additionalProps}
