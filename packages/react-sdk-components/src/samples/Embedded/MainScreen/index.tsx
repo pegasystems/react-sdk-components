a20cd37e3bbb7d7e6bdab16652cd1b88fcef04c0
SambhuPega <sambhu.ss@pega.com>
2025-10-29 15:56:55 +0530

    Added casetype in sdk-config for embedded and updated styles for Header

--- packages/react-sdk-components/src/samples/Embedded/MainScreen/index.tsx
+++ packages/react-sdk-components/src/samples/Embedded/MainScreen/index.tsx
@@ -5,6 +5,7 @@ import { makeStyles } from '@mui/styles';
 import StoreContext from '../../../bridge/Context/StoreContext';
 import createPConnectComponent from '../../../bridge/react_pconnect';
 
+import sdkConfigData from '../../../../../../sdk-config.json';
 import ShoppingOptionCard from '../ShoppingOptionCard';
 import ResolutionScreen from '../ResolutionScreen';
 import { shoppingOptions } from '../utils';
@@ -164,8 +165,7 @@ export default function MainScreen(props) {
     if (!mashupCaseType) {
       const caseTypes = PCore.getEnvironmentInfo()?.environmentInfoObject?.pyCaseTypeList;
       if (caseTypes && caseTypes.length > 0) {
-        //mashupCaseType = caseTypes[0].pyWorkTypeImplementationClassName;
-        mashupCaseType = 'DIXL-MediaCo-Work-PurchasePhone';
+        mashupCaseType = sdkConfigData.authConfig.mashupCaseType;
       }
     }
     let selectedPhoneGUID = '';
