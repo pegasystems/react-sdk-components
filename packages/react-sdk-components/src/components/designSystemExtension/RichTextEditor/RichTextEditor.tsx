5f892d77a9c00f0dca057269c05db95a566ee939
Sharma <vishalshrm539@gmail.com>
2026-06-04 15:12:19 +0530

    Fixed label not displaying as expected

--- packages/react-sdk-components/src/components/designSystemExtension/RichTextEditor/RichTextEditor.tsx
+++ packages/react-sdk-components/src/components/designSystemExtension/RichTextEditor/RichTextEditor.tsx
@@ -1,6 +1,6 @@
 import React, { forwardRef } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
-import { FormControl, FormHelperText, InputLabel, useTheme } from '@mui/material';
+import { FormControl, FormHelperText, FormLabel, useTheme } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import { useAfterInitialEffect, useConsolidatedRef, useUID } from '../../../hooks';
 
@@ -136,11 +136,11 @@ const RichTextEditor = forwardRef(function RichTextEditor(props: RichTextEditorP
   }
 
   return (
-    <FormControl variant='standard' data-test-id={testId} error={error} required={required}>
+    <FormControl variant='standard' data-test-id={testId} error={error} required={required} fullWidth>
       {!labelHidden && (
-        <InputLabel id='demo-simple-select-error-label' className={classes.fieldLabel}>
+        <FormLabel htmlFor={id} className={classes.fieldLabel}>
           {label}
-        </InputLabel>
+        </FormLabel>
       )}
       {richTextComponent}
       {info && <FormHelperText>{info}</FormHelperText>}
