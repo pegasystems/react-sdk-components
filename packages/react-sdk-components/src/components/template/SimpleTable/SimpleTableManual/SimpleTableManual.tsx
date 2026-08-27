4b4496d31bd7288cae4782d31307899efb2104fe
manasa <manasa.mashetty@in.pega.com>
2026-08-27 18:53:13 +0530

    updated SimpleTableManual component

--- packages/react-sdk-components/src/components/template/SimpleTable/SimpleTableManual/SimpleTableManual.tsx
+++ packages/react-sdk-components/src/components/template/SimpleTable/SimpleTableManual/SimpleTableManual.tsx
@@ -445,7 +445,7 @@ export default function SimpleTableManual(props: PropsWithChildren<SimpleTableMa
           const isDatapage = referenceListData.startsWith('D_');
           const pageReferenceValue = isDatapage
             ? `${referenceListData}[${index}]`
-            : `${pConn.getPageReference()}${referenceListData.substring(referenceListData.lastIndexOf('.'))}[${index}]`;
+            : `${pConn.getPageReference()}${referenceListStr}[${index}]`;
           const config = {
             meta: item,
             options: {
